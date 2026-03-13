7d22779d7f8792e12497ae28bf12b84dd5c18a7b
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2026-03-13 21:01:40 +0100

    make use of coresight-red

--- build/searchai/index.asset.php
+++ build/searchai/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0aa22649f1604aa57ce4');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '78a67aaa3474ee5c76cf');
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,23 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                '--color-primary': '#ef4444', // red-500
-                '--color-secondary': '#f472b6', // pink-400
+                '--color-primary': '#d62e2f',
+                '--color-secondary': '#f472b6',
                 '--color-accent': '#f472b6',
+                // Override Tailwind's default red scale so existing `bg-red-600`, `text-red-500` etc use the exact brand color organically
+                red: {
+                    50: '#fbf4f4',
+                    100: '#f6e8e8',
+                    200: '#eccbcb',
+                    300: '#e0a5a5',
+                    400: '#d07677',
+                    500: '#c55354',
+                    600: '#d62e2f', // Core brand red
+                    700: '#9b2021',
+                    800: '#811e1f',
+                    900: '#6d1d1f',
+                    950: '#3a0c0c',
+                },
             }
         },
     },
